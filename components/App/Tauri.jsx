64678408f414013194e19d30f9f0d3ecae58f322
Blookers <blookyl305@gmail.com>
2022-04-26 16:18:42 +1000

    Error Boundary for Tauri Applications

--- components/App/Tauri.jsx
+++ components/App/Tauri.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import { ErrorBoundary } from 'react-error-boundary';
+import { Back, ArrowClockwise, ExclamationTriangleFill } from 'react-bootstrap-icons';
 
 import Grid from '../Grid';
-
-export default function TauriApp(props) {
+import Button from '../Button';
+import Typography from '../Typography';
+export default function TauriApp({ css, children, ...props }) {
     return (
         <Grid width="100vw" height="100vh" direction="vertical" {...props} css={{
             maxWidth: '100vw',
@@ -10,9 +13,35 @@ export default function TauriApp(props) {
             overflow: 'hidden',
             maxHeight: '100vh',
             minHeight: '100vh',
-            borderRadius: 8
+            borderRadius: 8,
+            ...css
         }}>
-            {props.children}
+            <ErrorBoundary
+                FallbackComponent={({error, resetErrorBoundary, componentStack}) => (
+                    <Grid width="100%" height="100%" direction="vertical" alignItems="center" justifyContent="center">
+                        <Typography size="1.3rem" weight={700} family="Nunito" lineheight={1.5}>
+                            <ExclamationTriangleFill size={40} style={{ margin: '0 12px' }}/>
+                            Crashed!
+                        </Typography>
+                        <Typography size=".9rem" color="#ffffff80" margin="12px 0 0" family="Nunito">
+                            Looks like the application crashed unexpectedly...<br/>
+                            "{error.message}"
+                        </Typography>
+                        <Grid margin="1.5rem 0 0" spacing="8px">
+                            <Button onClick={() => location.reload()}>
+                                <ArrowClockwise/>
+                                Reload Application
+                            </Button>
+                            <Button theme="secondary" onClick={() => resetErrorBoundary()}>
+                                <Back/>
+                                Quick Refresh
+                            </Button>
+                        </Grid>
+                    </Grid>
+                )}
+            >
+                {children}
+            </ErrorBoundary>
         </Grid>
     );
 };
